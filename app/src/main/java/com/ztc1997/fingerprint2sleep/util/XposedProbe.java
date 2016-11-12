8e87d0b495b1498058131d7ddab3440b9d1fad91
ztc1997 <ztc2011@gmail.com>
2016-11-12 11:54:17 +0800

    Prepare for module version check

---
+++ app/src/main/java/com/ztc1997/fingerprint2sleep/util/XposedProbe.java
@@ -0,0 +1,7 @@
+package com.ztc1997.fingerprint2sleep.util;
+
+public class XposedProbe {
+    public static int activatedModuleVersion() {
+        return -1;
+    }
+}
--- app/src/main/kotlin/com/ztc1997/fingerprint2sleep/xposed/FPQAModule.kt
+++ app/src/main/kotlin/com/ztc1997/fingerprint2sleep/xposed/FPQAModule.kt
@@ -1,12 +1,15 @@
 package com.ztc1997.fingerprint2sleep.xposed
 
 import com.ztc1997.fingerprint2sleep.BuildConfig
+import com.ztc1997.fingerprint2sleep.util.XposedProbe
 import com.ztc1997.fingerprint2sleep.xposed.extention.tryAndPrintStackTrace
 import com.ztc1997.fingerprint2sleep.xposed.hook.AuthenticationCallbackHooks
 import com.ztc1997.fingerprint2sleep.xposed.hook.FingerprintServiceHooks
 import com.ztc1997.fingerprint2sleep.xposed.hook.SystemUIHooks
 import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class FPQAModule : IXposedHookLoadPackage {
@@ -26,6 +29,12 @@ class FPQAModule : IXposedHookLoadPackage {
             tryAndPrintStackTrace { FingerprintServiceHooks.doHook(lpp.classLoader) }
         } else if (lpp.packageName == "com.android.systemui" && lpp.processName == "com.android.systemui") {
             tryAndPrintStackTrace { SystemUIHooks.doHook(lpp.classLoader) }
+        } else if (lpp.packageName == BuildConfig.APPLICATION_ID) {
+            tryAndPrintStackTrace {
+                XposedHelpers.findAndHookMethod(XposedProbe::class.java.name, lpp.classLoader,
+                        XposedProbe::activatedModuleVersion.name,
+                        XC_MethodReplacement.returnConstant(BuildConfig.VERSION_CODE))
+            }
         }
 
         tryAndPrintStackTrace { AuthenticationCallbackHooks.doHook(lpp.classLoader) }
